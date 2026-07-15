81b97f25022eec60a6edccce4dd0ef09d6b75e04
msiva25012007 <msiva25012007@gmail.com>
2026-07-15 23:40:50 +0530

    Update UserServiceImpl.java

--- StudyBuddy_AI-Backend/backend/src/main/java/com/studybuddy/backend/service/UserServiceImpl.java
+++ StudyBuddy_AI-Backend/backend/src/main/java/com/studybuddy/backend/service/UserServiceImpl.java
@@ -6,58 +6,95 @@ import org.springframework.stereotype.Service;
 import com.studybuddy.backend.entity.User;
 import com.studybuddy.backend.repository.UserRepository;
 
+
 @Service
 public class UserServiceImpl implements UserService {
 
+
     private final UserRepository repo;
+
     private final PasswordEncoder passwordEncoder;
+
+
 
     public UserServiceImpl(UserRepository repo,
                            PasswordEncoder passwordEncoder) {
+
         this.repo = repo;
         this.passwordEncoder = passwordEncoder;
+
     }
+
+
 
     @Override
     public String register(User user) {
 
+
         System.out.println("Register API Called");
 
-        if (repo.findByEmail(user.getEmail()) != null) {
+
+        if (UserRepository.findByEmail(user.getEmail()) != null) {
+
             return "Email already exists";
+
         }
 
+
+
         if (user.getPassword().length() < 8) {
+
             return "Password must be at least 8 characters";
+
         }
+
+
 
         user.setPassword(
                 passwordEncoder.encode(user.getPassword())
         );
 
+
+
         repo.save(user);
 
+
+
         return "Registration Successful";
+
     }
+
+
+
+
 
     @Override
-    public String login(User user) {
+    public User login(User user) {
 
-        User existingUser = repo.findByEmail(user.getEmail());
 
-        if (existingUser == null) {
-            return "User not found";
+        User existingUser =
+                UserRepository.findByEmail(user.getEmail());
+
+
+
+        if(existingUser != null &&
+
+           passwordEncoder.matches(
+               user.getPassword(),
+               existingUser.getPassword()
+           )) {
+
+
+
+            return existingUser;
+
         }
 
-        boolean matched = passwordEncoder.matches(
-                user.getPassword(),
-                existingUser.getPassword()
-        );
 
-        if (matched) {
-            return "Login Successful";
-        }
 
-        return "Invalid Credentials";
+        return null;
+
     }
+
+
 }
